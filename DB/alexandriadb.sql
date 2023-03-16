5c3d1ba2b710bd2ec85d3664d658ae9aedec0cc4
kennyan11b <kennyan11b@gmail.com>
2023-03-16 16:41:58 -0700

    Finished DB Mapping

--- DB/alexandriadb.sql
+++ DB/alexandriadb.sql
@@ -16,6 +16,80 @@ CREATE SCHEMA IF NOT EXISTS `alexandriaDB` DEFAULT CHARACTER SET utf8 ;
 USE `alexandriaDB` ;
 
 -- -----------------------------------------------------
+-- Table `language`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `language` ;
+
+CREATE TABLE IF NOT EXISTS `language` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `author`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `author` ;
+
+CREATE TABLE IF NOT EXISTS `author` (
+  `id` INT NOT NULL,
+  `first_name` VARCHAR(45) NULL,
+  `middle_name` VARCHAR(45) NULL,
+  `last_name` VARCHAR(45) NULL,
+  `image_url` VARCHAR(2000) NULL,
+  `description` TEXT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `book`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `book` ;
+
+CREATE TABLE IF NOT EXISTS `book` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `cover_url` VARCHAR(2000) NULL,
+  `title` VARCHAR(100) NULL,
+  `description` TEXT NULL,
+  `pages` INT NULL,
+  `published_year` INT NULL,
+  `price` DECIMAL(6,2) NULL,
+  `isbn` CHAR(13) NULL,
+  `language_id` INT NOT NULL,
+  `author_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_book_language1_idx` (`language_id` ASC),
+  INDEX `fk_book_author1_idx` (`author_id` ASC),
+  CONSTRAINT `fk_book_language1`
+    FOREIGN KEY (`language_id`)
+    REFERENCES `language` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_book_author1`
+    FOREIGN KEY (`author_id`)
+    REFERENCES `author` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `genre`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `genre` ;
+
+CREATE TABLE IF NOT EXISTS `genre` (
+  `id` INT NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  `description` TEXT NULL,
+  `image_url` VARCHAR(2000) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
 -- Table `user`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `user` ;
@@ -26,8 +100,271 @@ CREATE TABLE IF NOT EXISTS `user` (
   `password` VARCHAR(45) NOT NULL,
   `enabled` TINYINT NOT NULL,
   `role` VARCHAR(45) NULL,
+  `image_url` VARCHAR(2000) NULL,
+  `first_name` VARCHAR(45) NULL,
+  `last_name` VARCHAR(45) NULL,
+  `about_me` TEXT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `username_UNIQUE` (`username` ASC))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `book_review`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `book_review` ;
+
+CREATE TABLE IF NOT EXISTS `book_review` (
+  `rating` INT NOT NULL,
+  `review` TEXT NULL,
+  `create_date` DATETIME NULL,
+  `last_update` DATETIME NULL,
+  `book_id` INT NOT NULL,
+  `user_id` INT NOT NULL,
+  INDEX `fk_review_book1_idx` (`book_id` ASC),
+  INDEX `fk_review_user1_idx` (`user_id` ASC),
+  PRIMARY KEY (`book_id`, `user_id`),
+  CONSTRAINT `fk_review_book1`
+    FOREIGN KEY (`book_id`)
+    REFERENCES `book` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_review_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `booklist`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `booklist` ;
+
+CREATE TABLE IF NOT EXISTS `booklist` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(2000) NOT NULL,
+  `description` TEXT NULL,
+  `image_url` VARCHAR(2000) NULL,
+  `user_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_booklist_user1_idx` (`user_id` ASC),
+  CONSTRAINT `fk_booklist_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `club`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `club` ;
+
+CREATE TABLE IF NOT EXISTS `club` (
+  `id` INT NOT NULL,
+  `name` VARCHAR(100) NOT NULL,
+  `description` TEXT NULL,
+  `owner_id` INT NOT NULL,
+  `create_date` DATETIME NULL,
+  `image_url` VARCHAR(2000) NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_club_user1_idx` (`owner_id` ASC),
+  CONSTRAINT `fk_club_user1`
+    FOREIGN KEY (`owner_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `booklist_has_book`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `booklist_has_book` ;
+
+CREATE TABLE IF NOT EXISTS `booklist_has_book` (
+  `booklist_id` INT NOT NULL,
+  `book_id` INT NOT NULL,
+  PRIMARY KEY (`booklist_id`, `book_id`),
+  INDEX `fk_booklist_has_book_book1_idx` (`book_id` ASC),
+  INDEX `fk_booklist_has_book_booklist1_idx` (`booklist_id` ASC),
+  CONSTRAINT `fk_booklist_has_book_booklist1`
+    FOREIGN KEY (`booklist_id`)
+    REFERENCES `booklist` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_booklist_has_book_book1`
+    FOREIGN KEY (`book_id`)
+    REFERENCES `book` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `user_has_club`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `user_has_club` ;
+
+CREATE TABLE IF NOT EXISTS `user_has_club` (
+  `user_id` INT NOT NULL,
+  `club_id` INT NOT NULL,
+  PRIMARY KEY (`user_id`, `club_id`),
+  INDEX `fk_user_has_club_club1_idx` (`club_id` ASC),
+  INDEX `fk_user_has_club_user1_idx` (`user_id` ASC),
+  CONSTRAINT `fk_user_has_club_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_has_club_club1`
+    FOREIGN KEY (`club_id`)
+    REFERENCES `club` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `user_has_favorite_book`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `user_has_favorite_book` ;
+
+CREATE TABLE IF NOT EXISTS `user_has_favorite_book` (
+  `user_id` INT NOT NULL,
+  `book_id` INT NOT NULL,
+  PRIMARY KEY (`user_id`, `book_id`),
+  INDEX `fk_user_has_book_book1_idx` (`book_id` ASC),
+  INDEX `fk_user_has_book_user1_idx` (`user_id` ASC),
+  CONSTRAINT `fk_user_has_book_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_has_book_book1`
+    FOREIGN KEY (`book_id`)
+    REFERENCES `book` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `club_has_book`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `club_has_book` ;
+
+CREATE TABLE IF NOT EXISTS `club_has_book` (
+  `club_id` INT NOT NULL,
+  `book_id` INT NOT NULL,
+  PRIMARY KEY (`club_id`, `book_id`),
+  INDEX `fk_club_has_book_book1_idx` (`book_id` ASC),
+  INDEX `fk_club_has_book_club1_idx` (`club_id` ASC),
+  CONSTRAINT `fk_club_has_book_club1`
+    FOREIGN KEY (`club_id`)
+    REFERENCES `club` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_club_has_book_book1`
+    FOREIGN KEY (`book_id`)
+    REFERENCES `book` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `book_has_genre`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `book_has_genre` ;
+
+CREATE TABLE IF NOT EXISTS `book_has_genre` (
+  `book_id` INT NOT NULL,
+  `genre_id` INT NOT NULL,
+  PRIMARY KEY (`book_id`, `genre_id`),
+  INDEX `fk_book_has_genre_genre1_idx` (`genre_id` ASC),
+  INDEX `fk_book_has_genre_book1_idx` (`book_id` ASC),
+  CONSTRAINT `fk_book_has_genre_book1`
+    FOREIGN KEY (`book_id`)
+    REFERENCES `book` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_book_has_genre_genre1`
+    FOREIGN KEY (`genre_id`)
+    REFERENCES `genre` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `club_comment`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `club_comment` ;
+
+CREATE TABLE IF NOT EXISTS `club_comment` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `club_comment` TEXT NULL,
+  `comment_date` DATETIME NULL,
+  `user_id` INT NOT NULL,
+  `club_id` INT NOT NULL,
+  `in_reply_to_id` INT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_club_comment_user1_idx` (`user_id` ASC),
+  INDEX `fk_club_comment_club1_idx` (`club_id` ASC),
+  INDEX `fk_club_comment_club_comment1_idx` (`in_reply_to_id` ASC),
+  CONSTRAINT `fk_club_comment_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_club_comment_club1`
+    FOREIGN KEY (`club_id`)
+    REFERENCES `club` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_club_comment_club_comment1`
+    FOREIGN KEY (`in_reply_to_id`)
+    REFERENCES `club_comment` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `book_comment`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `book_comment` ;
+
+CREATE TABLE IF NOT EXISTS `book_comment` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `book_comment` TEXT NULL,
+  `comment_date` DATETIME NULL,
+  `book_id` INT NOT NULL,
+  `user_id` INT NOT NULL,
+  `in_reply_to_id` INT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_book_comment_book1_idx` (`book_id` ASC),
+  INDEX `fk_book_comment_user1_idx` (`user_id` ASC),
+  INDEX `fk_book_comment_book_comment1_idx` (`in_reply_to_id` ASC),
+  CONSTRAINT `fk_book_comment_book1`
+    FOREIGN KEY (`book_id`)
+    REFERENCES `book` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_book_comment_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_book_comment_book_comment1`
+    FOREIGN KEY (`in_reply_to_id`)
+    REFERENCES `book_comment` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 SET SQL_MODE = '';
@@ -46,7 +383,7 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `alexandriaDB`;
-INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `role`) VALUES (1, 'admin', 'admin', 1, NULL);
+INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `role`, `image_url`, `first_name`, `last_name`, `about_me`) VALUES (1, 'admin', 'admin', 1, NULL, NULL, NULL, NULL, NULL);
 
 COMMIT;
 
