dd081ae08db07beda3f584e06709ba442eba0e91
kennyan11b <kennyan11b@gmail.com>
2023-03-16 13:13:52 -0700

    Congifure Hibernate, test user entity

--- JPAAlexandria/src/main/java/com/skilldistillery/alexandria/entities/User.java
+++ JPAAlexandria/src/main/java/com/skilldistillery/alexandria/entities/User.java
@@ -1,5 +1,7 @@
 package com.skilldistillery.alexandria.entities;
 
+import java.util.Objects;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -8,6 +10,86 @@ import javax.persistence.Id;
 @Entity
 public class User {
 
-	@Id @GeneratedValue(strategy= GenerationType.IDENTITY)
+	@Id 
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
 	private int id;
+	
+	private String username;
+	
+	private String password;
+	
+	private boolean	enabled;
+	
+	private String role;
+
+	public User() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("User [id=").append(id).append(", username=").append(username).append(", password=")
+				.append(password).append(", enabled=").append(enabled).append(", role=").append(role).append("]");
+		return builder.toString();
+	}
+	
+	
 }
