7674a6224eb5afcd43889ec8da13c2ee86d672b1
andre_lacommare <alacommaresb@gmail.com>
2023-03-17 09:30:31 -0700

    ClubComment mapped, JUnit Test added for ClubComment, Book, Author, and Language

--- JPAAlexandria/src/main/java/com/skilldistillery/alexandria/entities/ClubComment.java
+++ JPAAlexandria/src/main/java/com/skilldistillery/alexandria/entities/ClubComment.java
@@ -1,6 +1,7 @@
 package com.skilldistillery.alexandria.entities;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -22,6 +23,54 @@ public class ClubComment {
 	
 	@Column(name="comment_date")
 	private LocalDate commentDate;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public LocalDate getCommentDate() {
+		return commentDate;
+	}
+
+	public void setCommentDate(LocalDate commentDate) {
+		this.commentDate = commentDate;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ClubComment other = (ClubComment) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "ClubComment [id=" + id + ", comment=" + comment + ", commentDate=" + commentDate + "]";
+	}
+	
+	
 	
 	
 	
